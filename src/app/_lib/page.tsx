bdecd3a9a3638a128ec80a2e8d13e8b638c578d7
Matthieu <matthieu.degny.dev@gmail.com>
2024-08-03 18:34:01 +0200

    private folder

---
+++ src/app/_lib/page.tsx
@@ -0,0 +1,5 @@
+// _lib, _ signifie que ce fichier est un fichier privé et ne doit pas être accessible directement dans le navigateur
+
+export default function PrivateRoute() {
+  return <h1>You cannot view this in the browser</h1>;
+}
