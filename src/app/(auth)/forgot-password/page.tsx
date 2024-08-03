a128aa7187138d02cd9501009a30979986e60267
Matthieu <matthieu.degny.dev@gmail.com>
2024-08-03 18:30:16 +0200

    route groups auth

---
+++ src/app/(auth)/forgot-password/page.tsx
@@ -0,0 +1,7 @@
+// le folder (auth) permet de regrouper les pages qui sont liées à l'authentification de l'utilisateur
+// Organiser les fichiers de votre projet sans affecter la structure de l'URL
+// donc pour accéder à cette page il faut diriger vers /forgot-password à l'URL de base et non pas /auth/forgot-password
+
+export default function ForgotPassword() {
+  return <h1>Forgot password</h1>;
+}
