b31ec7f771b134fb0de4f700105293123841de91
Matthieu <matthieu.degny.dev@gmail.com>
2024-08-04 09:51:09 +0200

    add doc layout

--- src/app/complex-dashboard/layout.tsx
+++ src/app/complex-dashboard/layout.tsx
@@ -1,3 +1,19 @@
+// Utilisation du layout vs page :
+
+// Le layout est idéal pour définir une structure commune qui s'applique à plusieurs pages ou composants.
+// La page (page.tsx) est généralement utilisée pour le contenu spécifique à une route particulière.
+
+// Avantages de l'utilisation du layout :
+
+// Réutilisation : Le layout permet de définir une structure commune réutilisable.
+// Chargement parallèle : Les composants définis dans les dossiers @ peuvent être chargés en parallèle, améliorant potentiellement les performances.
+// Séparation des préoccupations : Chaque composant peut être défini et géré séparément.
+
+// Quand utiliser le layout :
+
+// Pour des éléments d'interface utilisateur partagés entre plusieurs pages (comme des menus, des en-têtes, des pieds de page).
+// Pour organiser des composants qui doivent être chargés indépendamment.
+
 export default function ComplexDashboardLayout({
   children,
   users,
