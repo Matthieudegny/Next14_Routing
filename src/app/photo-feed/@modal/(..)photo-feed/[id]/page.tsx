fc2966b151874e6a6b5844ad6ce6d73c7addd801
Matthieu <matthieu.degny.dev@gmail.com>
2024-08-04 10:34:10 +0200

    end

---
+++ src/app/photo-feed/@modal/(..)photo-feed/[id]/page.tsx
@@ -0,0 +1,30 @@
+// je souhaite intercepter la route photo-feed, elle comprend une rouet paramétrée [id] qui affiche une photo en grand format
+//  donc ma route pour intercepter sera /(..)photo-feed/[id]
+
+import Image from "next/image";
+import wondersImages, { WonderImage } from "../../../wonders";
+import Modal from "../../../../../components/modal";
+
+export default function PhotoModal({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  const photo: WonderImage = wondersImages.find((p) => p.id === id)!;
+
+  return (
+    <Modal>
+      <Image
+        alt={photo.name}
+        src={photo.src}
+        className="w-full object-cover aspect-square"
+      />
+
+      <div className="bg-white p-4">
+        <h2 className="text-xl font-semibold">{photo.name}</h2>
+        <h3>{photo.photographer}</h3>
+        <h3>{photo.location}</h3>
+      </div>
+    </Modal>
+  );
+}
